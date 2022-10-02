bda662c0b1fb1fa913f2e02199876213cec1225c
Nam Tang <nam.tang@live.fr>
2022-10-02 19:09:25 +0200

    styles guide added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ['index.html'],
   theme: {
     extend: {},
+    colors: {
+      'Bright-orange': 'hsl(31, 77%, 52%)',
+      'Dark-cyan': 'hsl(-84, 100%, 22%)',
+      'Very-dark-cyan': 'hsl(179, 100%, 13%)',
+      'paragraphs': 'hsla(0, 0%, 100%, 0.75)',
+      'background-headings-buttons': 'hsl(0, 0%, 95%)'
+    }
   },
   plugins: [],
 }
