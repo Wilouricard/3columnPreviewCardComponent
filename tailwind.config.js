5a9ac16e5717c59eaa4df4557241ea94475d5136
Nam Tang <nam.tang@live.fr>
2022-10-03 14:36:28 +0200

    final

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,11 @@ module.exports = {
     extend: {},
     colors: {
       'Bright-orange': 'hsl(31, 77%, 52%)',
-      'Dark-cyan': 'hsl(-84, 100%, 22%)',
+      'Dark-cyan': '#008B8B',
       'Very-dark-cyan': 'hsl(179, 100%, 13%)',
       'paragraphs': 'hsla(0, 0%, 100%, 0.75)',
-      'background-headings-buttons': 'hsl(0, 0%, 95%)'
+      'background-headings-buttons': 'hsl(0, 0%, 95%)',
+      'white': '#FFFFFF'
     }
   },
   plugins: [],
